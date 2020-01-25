d7266ac11067d248eed1898338f778d1f01fda7e
Elkin Omar Jimenez Garcia <elomjiga@hotmail.com>
2020-01-25 17:43:28 -0500

    Creando una iteración dependiendo de una lista

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+
+    super();
+    this.state = {
+
+      usuarios: [
+        {
+          nombre: 'Elkin',
+          correo: 'elkin.jiga@hotmail.com',
+          enlace: 'elkin.com'
+        },
+        {
+          nombre: 'Omar',
+          correo: 'omar.jiga@hotmail.com',
+          enlace: 'omar.com'
+        },
+      ]
+
+    }
+
+  }
+
+  ponerFilas = () => (
+
+    this.state.usuarios.map((usuario) => (
+
+      <tr>
+        <td>{usuario.nombre}</td>
+        <td>{usuario.correo}</td>
+        <td>{usuario.enlace}</td>
+      </tr>
+
+    ))
+
+  )
+
+  render() {
+    return (
+      <div className="margen" >
+        <table className="tabla">
+          <thead>
+            <tr>
+              <th>Nombre</th>
+              <th>Correo</th>
+              <th>Enlace</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.ponerFilas()}
+          </tbody>
+        </table>
+      </div>
+
+    );
+  }
+
 }
 
 export default App;
